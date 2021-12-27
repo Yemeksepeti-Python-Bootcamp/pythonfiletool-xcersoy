6abf48bbaa4f1d10cfc8007eea4d088fd86c1693
Can Ersoy <canersoy997@gmail.com>
2021-12-27 23:25:53 +0300

    Add FileTool class and define Menu method

--- FileTool.py
+++ FileTool.py
@@ -1,1 +1,10 @@
+class FileTool():
+    def __init__(self):
+        print("Konsol islemleri icin Menu() metodunu, dosya islemleri icin FileOperations() metodunu cagiriniz.")
+    def Menu(self,path):
+        with open(path) as file:
+            content = file.readlines()
+        input_ = input("Dosyada arama yapmak icin 's', silme yapmak icin 'd', ekleme yapmak icin 'a', guncelleme yapmak icin 'u' karakterini yaziniz: \n")
 
+fileTool = FileTool()
+fileTool.Menu("letter_frequency.csv")
