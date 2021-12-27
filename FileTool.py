585db6fe7d8d4ab19184bf6fde37a19995b0729d
Can Ersoy <canersoy997@gmail.com>
2021-12-28 01:27:49 +0300

    Add append function to Menu() method

--- FileTool.py
+++ FileTool.py
@@ -4,11 +4,11 @@ class FileTool():
     def Menu(self,path):
         with open(path,"r+") as file:
             content = file.readlines()
-            input_ = input("Dosyada arama yapmak icin 's', silme yapmak icin 'd', ekleme yapmak icin 'a', guncelleme yapmak icin 'u' karakterini yaziniz: \n")
+            input_ = input("Dosyada arama yapmak icin 's', silme yapmak icin 'd', ekleme yapmak icin 'a', guncelleme yapmak icin 'u' karakterini yaziniz:\n")
             if input_ == "s":
                 counter = 0
                 searchStrLines = []
-                searchStr = input("Dosyada aranacak metni yaziniz: \n")
+                searchStr = input("Dosyada aranacak metni yaziniz:\n")
                 for line in content:
                     if searchStr in line:
                         counter += 1
@@ -20,16 +20,23 @@ class FileTool():
                 file.truncate()
                 deleteOption = input("Satir numarasina gore silmek icin 'n', girilecek metne gore silmek icin 't' karakterini yaziniz:\n")
                 if deleteOption == "n":
-                    linesToDelete = [int(x) for x in input("Silinecek satir numarasini veya numaralarini arada virgul olacak sekilde giriniz: ").split(',')]
+                    lineNoToDelete = [int(x) for x in input("Silinecek satir numarasini veya numaralarini arada virgul olacak sekilde giriniz: ").split(',')]
                     for number, line in enumerate(content):
-                        if number+1 not in linesToDelete:
+                        if number+1 not in lineNoToDelete:
                             file.write(line)
                 if deleteOption == "t":
-                    linesToDelete = [x for x in input("Girilecek metni veya metinleri iceren satirlarin silinecegi metni veya metinleri arada virgul olacak sekilde giriniz:\n").split(',')]
-                    for searchWord in linesToDelete:
+                    wordToDelete = [x for x in input("Girilecek metni veya metinleri iceren satirlarin silinecegi metni veya metinleri arada virgul olacak sekilde giriniz:\n").split(',')]
+                    for word in wordToDelete:
                         for number, line in enumerate(content):
-                            if searchWord not in line:
+                            if word not in line:
                                 file.write(line)
+            if input_ == "a":
+                lineToAppend = input("Dosyaya eklenecek satiri giriniz:\n")
+                while True:
+                    file_eof = file.read()
+                    if file_eof == '':
+                        file.write("\n"+lineToAppend)
+                        break
 
 fileTool = FileTool()
 fileTool.Menu("letter_frequency.csv")
