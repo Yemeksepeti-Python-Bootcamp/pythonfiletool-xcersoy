0a936a74e92bfb8dfaa98c3e01f9d638de4cec7d
Can Ersoy <canersoy997@gmail.com>
2021-12-27 23:44:43 +0300

    Update search function of Menu() method to also display the lines that contain the given str

--- FileTool.py
+++ FileTool.py
@@ -7,11 +7,14 @@ class FileTool():
         input_ = input("Dosyada arama yapmak icin 's', silme yapmak icin 'd', ekleme yapmak icin 'a', guncelleme yapmak icin 'u' karakterini yaziniz: \n")
         if input_ == "s":
             counter = 0
+            searchStrLines = []
             searchStr = input("Dosyada aranacak metni yaziniz: \n")
             for line in content:
                 if searchStr in line:
                     counter += 1
-            print(f"{searchStr} : {counter}")
-            
+                    searchStrLines.append(line)
+            print(f"{searchStr} kelimesi {path} dosyasında {counter} kere asagidaki satirlarda gecmektedir:")
+            print(*searchStrLines,end="\n")
+
 fileTool = FileTool()
 fileTool.Menu("letter_frequency.csv")
