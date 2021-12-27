319d45ee41114ed3a54b0537d6e927061dacdd49
Can Ersoy <canersoy997@gmail.com>
2021-12-28 00:48:12 +0300

    Add line(s) to delete by line number feature to delete function of Main()method

--- FileTool.py
+++ FileTool.py
@@ -2,25 +2,30 @@ class FileTool():
     def __init__(self):
         print("Konsol islemleri icin Menu() metodunu, dosya islemleri icin FileOperations() metodunu cagiriniz.")
     def Menu(self,path):
-        with open(path) as file:
+        with open(path,"r+") as file:
             content = file.readlines()
-        input_ = input("Dosyada arama yapmak icin 's', silme yapmak icin 'd', ekleme yapmak icin 'a', guncelleme yapmak icin 'u' karakterini yaziniz: \n")
-        if input_ == "s":
-            counter = 0
-            searchStrLines = []
-            searchStr = input("Dosyada aranacak metni yaziniz: \n")
-            for line in content:
-                if searchStr in line:
-                    counter += 1
-                    searchStrLines.append(line)
-            print(f"{searchStr} metni {path} dosyasında {counter} kere asagidaki satirlarda gecmektedir:")
-            print(*searchStrLines,end="\n")
-        if input_ == "d":
-            deleteOption = input("Satir numarasina gore silmek icin 'n', girilecek metne gore silmek icin 't' karakterini yaziniz.")
-            if deleteOption == "n":
-                pass
-            if deleteOption == "t":
-                pass
+            input_ = input("Dosyada arama yapmak icin 's', silme yapmak icin 'd', ekleme yapmak icin 'a', guncelleme yapmak icin 'u' karakterini yaziniz: \n")
+            if input_ == "s":
+                counter = 0
+                searchStrLines = []
+                searchStr = input("Dosyada aranacak metni yaziniz: \n")
+                for line in content:
+                    if searchStr in line:
+                        counter += 1
+                        searchStrLines.append(line)
+                print(f"{searchStr} metni {path} dosyasında {counter} kere asagidaki satirlarda gecmektedir:")
+                print(*searchStrLines,end="\n")
+            if input_ == "d":
+                file.seek(0)
+                file.truncate()
+                deleteOption = input("Satir numarasina gore silmek icin 'n', girilecek metne gore silmek icin 't' karakterini yaziniz:\n")
+                if deleteOption == "n":
+                    linesToDelete = [int(x) for x in input("Silinecek satir numarasini veya numaralarini arada bosluk olacak sekilde giriniz: ").split()]
+                    for number, line in enumerate(content):
+                        if number+1 not in linesToDelete:
+                            file.write(line)
+                if deleteOption == "t":
+                    pass
 
 fileTool = FileTool()
 fileTool.Menu("letter_frequency.csv")
