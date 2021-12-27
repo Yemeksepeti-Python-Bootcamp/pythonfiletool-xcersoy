82b9fe602dbdcdd66d5c536cc7ae93b32cf5f974
Can Ersoy <canersoy997@gmail.com>
2021-12-27 23:35:44 +0300

    Add search function to Menu() method

--- FileTool.py
+++ FileTool.py
@@ -5,6 +5,13 @@ class FileTool():
         with open(path) as file:
             content = file.readlines()
         input_ = input("Dosyada arama yapmak icin 's', silme yapmak icin 'd', ekleme yapmak icin 'a', guncelleme yapmak icin 'u' karakterini yaziniz: \n")
-
+        if input_ == "s":
+            counter = 0
+            searchStr = input("Dosyada aranacak metni yaziniz: \n")
+            for line in content:
+                if searchStr in line:
+                    counter += 1
+            print(f"{searchStr} : {counter}")
+            
 fileTool = FileTool()
 fileTool.Menu("letter_frequency.csv")
